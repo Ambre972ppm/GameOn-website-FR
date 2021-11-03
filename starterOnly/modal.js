419f412ba607904d9d3096cba73745f58b04a180
Ambre GONDRY <gondry.ambre@gmail.com>
2021-11-03 20:19:17 +0100

    ajout bouton fermer modal confirmation + reprise fichier modal js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,14 @@
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const closeConfirmation = document.querySelector(".closeConfirmation");
+const btnClose = document.querySelector('#btnClose');
+const btnSubmit = document.querySelector("#btnSubmit");
+const form = document.querySelector("form");
+const modalConfirmation = document.querySelector(".modal-confirmation");
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,27 +18,16 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelector(".close");
-const btnSubmit = document.querySelector("#btnSubmit");
-const form = document.querySelector("form");
-
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-//close modal event
-closeBtn.addEventListener("click", closeModal);
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 //close modal form
 function closeModal() {
     modalbg.style.display = "none";  
 }
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//close modal event
+closeBtn.addEventListener("click", closeModal);
